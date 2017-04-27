7d23c52b453259f0b65e53f1d23d3cf986252741
Seven Starosta <sevenstarosta@gmail.com>
2017-04-27 15:08:02 -0400

    Should be finished with code. Still need to add in resize script

--- ModelTrainer.py
+++ ModelTrainer.py
@@ -12,35 +12,35 @@ import numpy
 
 class contentLoss(losses.Loss):
     #pass cmodel.input as array. Pass model. Pass already predicted output.
-    def __init__(self,model,input,predicted):
+    def __init__(self,layer,predicted):
         super(contentLoss,self).__init__()
         self.name="Content Loss Function"
-        self.model=model
-        self.input=input
+        self.layer=layer
         self.predicted=predicted
         
     def build_loss(self):
-        A=self.model.predict(numpy.array([self.input]))
+        A=self.layer.get_output_at(0)
         B=self.predicted
         (l,x,y,N)=A.shape
         C=0
-        for map in range(N):
-            for a in range(x):
-                for b in range(y):
+        for a in range(x):
+            for b in range(y):
+                for map in range(N):
                     C+=(A[0][a][b][map]-B[0][a][b][map])**2
         C/=2
         return C
         
 class styleLoss(losses.Loss):
-    def __init__(self,model,input,predicted):
+    #predicted is already in gram matrix form
+    def __init__(self,layer,predicted):
         super(styleLoss,self).__init__()
         self.name="Style Loss Function"
-        self.model=model
-        self.input=input
+        self.layer=layer
         self.predicted=predicted
         
     def build_loss(self):
-        pred=self.model.predict(numpy.array([self.input]))
+        #need to build 4d array from layer.
+        pred=self.layer.get_output_at(0)
         (l,x,y,N)=pred.shape
         A=gramMat(pred)
         C=0
@@ -61,7 +61,6 @@ def gramMat(A):
     return B
 
 #change to pass in input image. Then run predict on truncated model.
-'Make B be the predict on the original style image, resized to the size of the content image'
     
 def whiteNoise(rows,cols):
     noise_matrix = numpy.empty(shape=[rows,cols,3], dtype= numpy.uint8)
@@ -72,16 +71,15 @@ def whiteNoise(rows,cols):
             noise_matrix[r][c][2] = randint(200,255)
     return noise_matrix
 
-img = image.load_img('d53.jpg')
+img = image.load_img('eiffel128.png')
 contentImage=image.img_to_array(img)
 
 noise=whiteNoise(contentImage.shape[0],contentImage.shape[1])
 
-img = image.load_img('starrynight256.jpg')
+img = image.load_img('starrynight128.jpg')
 styleImage=image.img_to_array(img)
 #styleImage=cv2.resize(styleImage,None,(contentImage.shape[0],contentImage.shape[1]))
 #resize styleimage
-
 
 input_tensor = Input(shape=contentImage.shape)
 
@@ -89,31 +87,42 @@ trainedModel = VGG19(weights='imagenet',include_top= False,input_tensor=input_te
 
 layer_dict = dict([(layer.name, layer) for layer in trainedModel.layers[1:]])
 
-contentModel = Sequential()
-#print(trainedModel.summary())
+contentModel = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+for i in range(8):
+    contentModel.layers.pop()
+contentModel.outputs = [contentModel.layers[-1].output]
+contentModel.layers[-1].outbound_nodes = []
 
-for layer in trainedModel.layers[:14]:
-    contentModel.add(layer)
-    
-styleModel1 = Sequential()
-for layer in trainedModel.layers[:2]:
-    styleModel1.add(layer)
-    
-styleModel2 = Sequential()
-for layer in trainedModel.layers[:5]:
-    styleModel2.add(layer)
-    
-styleModel3 = Sequential()
-for layer in trainedModel.layers[:8]:
-    styleModel3.add(layer)
-    
-styleModel4 = Sequential()
-for layer in trainedModel.layers[:13]:
-    styleModel4.add(layer)
-    
-#styleModel5 = Sequential()
-#for layer in trainedModel.layers[:8]:
-#styleModel5.add(layer)
+styleModel1 = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+for i in range(20):
+    styleModel1.layers.pop()
+styleModel1.outputs = [styleModel1.layers[-1].output]
+styleModel1.layers[-1].outbound_nodes = []
+
+styleModel2 = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+for i in range(17):
+    styleModel2.layers.pop()
+styleModel2.outputs = [styleModel2.layers[-1].output]
+styleModel2.layers[-1].outbound_nodes = []
+
+styleModel3 = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+for i in range(14):
+    styleModel3.layers.pop()
+styleModel3.outputs = [styleModel3.layers[-1].output]
+styleModel3.layers[-1].outbound_nodes = []
+
+styleModel4 = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+for i in range(9):
+    styleModel4.layers.pop()
+styleModel4.outputs = [styleModel4.layers[-1].output]
+styleModel4.layers[-1].outbound_nodes = []
+
+#block5_conv1, needt to repalce 8 with appropriate index offset
+#styleModel5 = VGG19(weights='imagenet',include_top= False,input_tensor=input_tensor)
+#for i in range(8):
+#    styleModel5.layers.pop()
+#styleModel5.outputs = [styleModel5.layers[-1].output]
+#styleModel5.layers[-1].outbound_nodes = []
     
 #must create grammat of output of style image on style model
 contentBase = contentModel.predict(numpy.array([contentImage]))
@@ -123,35 +132,19 @@ styleBase3 = gramMat(styleModel3.predict(numpy.array([styleImage])))
 styleBase4 = gramMat(styleModel4.predict(numpy.array([styleImage])))
 #styleBase5 = gramMat(styleModel5.predict(numpy.array([styleImage])))
 
+#sum style weights so that total approx = 1000
 losses = [
-    (contentLoss(contentModel,contentModel.input,contentBase),1),
-    (styleLoss(styleModel1,contentModel.input,styleBase1),1000),
-    (styleLoss(styleModel2,contentModel.input,styleBase2),1000),
-    (styleLoss(styleModel3,contentModel.input,styleBase3),1000),
-    (styleLoss(styleModel4,contentModel.input,styleBase4),1000)
+    (contentLoss(layer_dict["block4_conv2"],contentBase),1),
+    (styleLoss(layer_dict["block1_conv1"],styleBase1),250),
+    (styleLoss(layer_dict["block2_conv1"],styleBase2),250),
+    (styleLoss(layer_dict["block3_conv1"],styleBase3),250),
+    (styleLoss(layer_dict["block4_conv1"],styleBase4),250)
 ]
-opt = Optimizer(contentModel.input,losses)    
-finaloutput=opt.minimize(seed_img=noise,max_iter=25,verbose=True)[0]
+
+opt = Optimizer(trainedModel.input,losses)
+print("starting optimization")    
+finaloutput=opt.minimize(seed_img=noise,max_iter=50,verbose=False,progress_gif_path='proggif.gif')[0]
 print("finished")
 pyplot.imshow(finaloutput)
 pyplot.show()
 print("done with showing")
-
-#print((contentBase[0]).shape)
-#print(trainedModel.summary())
-#for x in trainedModel.get_weights():
-#    print(x.shape)
-    
-#print(contentImage.shape)
-#print(contentModel.predict(numpy.array([contentImage]),1,0))
-
-#print("SHAPE")
-#appears to have the outputs of 512 neurons, which each roughly preserve aspect ratio of input image.
-#print(contentModel.predict(numpy.array([contentImage]),1,0).shape)
-#may be easier to convert...
-#firstlayer=trainedModel.layers[1]
-#print(firstlayer.get_weights())
-
-#mean squared error for content.
-
-trainedModel.save('trainedModel1.HDF5')
